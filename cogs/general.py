9f7c701e9273c1e9abc14413dabbc8d940eb3019
pmqueiroz <pedroqueiroz593@gmail.com>
2020-07-11 15:29:51 -0300

    fix sayd response

--- cogs/general.py
+++ cogs/general.py
@@ -29,7 +29,6 @@ class General(commands.Cog):
         def is_author(ctx):
             return ctx.author
         await ctx.channel.purge(limit=1)
-        await asyncio.sleep(5)
         await ctx.send(arg)
 
     @commands.command()
