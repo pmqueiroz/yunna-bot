526025700b6b676073f260a0fd4f814529ee3b76
pmqueiroz <pedroqueiroz593@gmail.com>
2020-07-19 10:33:34 -0300

    fix

--- cogs/useful.py
+++ cogs/useful.py
@@ -1,23 +1,23 @@
-# import discord
-# from discord.ext import flags, commands
-# from googletrans import Translator #googletrans 3.0.0
+import discord
+from discord.ext import flags, commands
+from googletrans import Translator
 
-# class useful(commands.Cog):
+class useful(commands.Cog):
 
-#     def __init__(self, bot):
-#         self.bot = bot
+    def __init__(self, bot):
+        self.bot = bot
 
-#     @flags.add_flag("--lan", default='en')
-#     @flags.add_flag("msg", nargs='+')
-#     @flags.command()
-#     async def translate(self, ctx, **flags):
-#         arg = flags["msg"]
-#         message = ' '.join(arg)
-#         dest = flags["lan"]
-#         translator = Translator(service_urls=['translate.google.com'])
-#         translation = translator.translate(message, dest=dest)
-#         await ctx.channel.send(translation.text)
+    # @flags.add_flag("--lan", default='en')
+    # @flags.add_flag("msg", nargs='+')
+    # @flags.command()
+    # async def translate(self, ctx, **flags):
+    #     arg = flags["msg"]
+    #     message = ' '.join(arg)
+    #     dest = flags["lan"]
+    #     translator = Translator(service_urls=['translate.google.com'])
+    #     translation = translator.translate(message, dest=dest)
+    #     await ctx.channel.send(translation.text)
 
 
-# def setup(bot):
-#     bot.add_cog(useful(bot))
+def setup(bot):
+    bot.add_cog(useful(bot))
