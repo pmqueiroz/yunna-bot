e90ebc1f8b54a52f89cdf7a76f581db0c5a63532
pmqueiroz <pedroqueiroz593@gmail.com>
2020-07-20 12:10:02 -0300

    fix

--- cogs/useful.py
+++ cogs/useful.py
@@ -7,16 +7,16 @@ class useful(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    # @flags.add_flag("--lan", default='en')
-    # @flags.add_flag("msg", nargs='+')
-    # @flags.command()
-    # async def translate(self, ctx, **flags):
-    #     arg = flags["msg"]
-    #     message = ' '.join(arg)
-    #     dest = flags["lan"]
-    #     translator = Translator(service_urls=['translate.google.com'])
-    #     translation = translator.translate(message, dest=dest)
-    #     await ctx.channel.send(translation.text)
+    @flags.add_flag("--lan", default='en')
+    @flags.add_flag("msg", nargs='+')
+    @flags.command()
+    async def translate(self, ctx, **flags):
+        arg = flags["msg"]
+        message = ' '.join(arg)
+        dest = flags["lan"]
+        translator = Translator(service_urls=['translate.google.com'])
+        translation = translator.translate(message, dest=dest)
+        await ctx.channel.send(translation.text)
 
 
 def setup(bot):
